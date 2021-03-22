d1d9d704483a1e97c48af63be6f3f5f85f025265
Anderson Marlon <anderson18_marlon@hotmail.com>
2021-03-22 03:15:09 -0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const selectElement = (selected) => document.querySelector(selected);
+
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
